ea072c801d5060ba5e938fa34eaf97f63a9de18d
Phillip <phillipkregg@gmail.com>
2013-09-28 03:11:26 +0000

    Initial commit - setup Rails and Made Backbone model for images, a view and a collection for images

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestApp::Application.config.secret_key_base = '9bacb8a4e5d776ab4077207d96c89687102ee588457e27c1f6955a46bf50453474ac9fa51efd91fe68eeaccd7db87017591b7a47f5a32d9d7de2419d55d88022'
